02a07d42166f99e4f9d279477338841de5771d31
andrew-belz <andrewbelzjr@gmail.com>
2026-01-31 15:38:57 -0700

    wrote a short R demo

--- docs/Rdemo.R
+++ docs/Rdemo.R
@@ -1,14 +1,19 @@
-#install.packages("tidyverse") #uncomment and run this line to install the package
+#uncomment and run this line to install the package
+#install.packages("tidyverse") 
+#install.packages("igraph")
 library(tidyverse) #import the package
 
-# initialize the data frame
+#initialize the data frame
 df <- data.frame("x" = seq(1, 10), "y" = seq(1, 10))
 
+#scatterplot
 ggplot(df, aes(x = x, y = y)) + 
-geom_point(size = 2) +
+geom_point(size = 2, color = "magenta") + # plot the scatter plot
 labs(
     xlab = "X",
     ylab = "Y",
     title = "Y = X"
 ) + 
-theme_minimal()
+theme_minimal() + # try the different themes
+# fit a linear model
+geom_smooth(method = "lm", se = FALSE, color = "maroon") 

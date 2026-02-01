016b499a09db911846f6d5c89e3b5be109f5b63b
andrew-belz <andrewbelzjr@gmail.com>
2026-01-31 17:14:01 -0700

    started working on the betti numbers module

--- src/clique_homology/betti_numbers_api.py
+++ src/clique_homology/betti_numbers_api.py
@@ -1,4 +1,6 @@
 # Unimplemented
 
+import networkit as nk
+
 def betti_numbers(vertices: set[int], edges: list[tuple[int, int]], *, max_dim=2, clique_policy="uncolored") -> tuple[int, ...]:
     raise NotImplementedError

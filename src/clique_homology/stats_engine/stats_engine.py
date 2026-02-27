f7233475a0ad478c8796aac8a3fc7e91db3d90c7
andrew-belz <andrewbelzjr@gmail.com>
2026-02-26 17:08:13 -0700

    wrote some code to generate visualizations for src presentation

--- src/clique_homology/stats_engine/stats_engine.py
+++ src/clique_homology/stats_engine/stats_engine.py
@@ -5,7 +5,7 @@
 from clique_homology.stats_engine.betti_numbers import betti_numbers
 from clique_homology.stats_engine.null_distribution import null_distribution
 # change this back to p_values instead
-from clique_homology.stats_engine.p_val_sanity_check import calculate_p_vector
+from clique_homology.stats_engine.p_values import calculate_p_vector
 from networkit import Graph, nxadapter
 import numpy as np
 
@@ -13,9 +13,9 @@ def stats_engine(G:Graph, colors:list[str], iters:int=100, allowed_colors: list[
     # out: list of numpy arrays
     null_dist = np.array(null_distribution(graph=G, coloring=colors, iterations=iters, allowed_colors=allowed_colors))
     obs_betti = betti_numbers(G=G, colors=colors, allowed_colors=allowed_colors)
-    pval = calculate_p_vector(obs_betti, null_dist)
+    pval, obs, dist = calculate_p_vector(obs_betti, null_dist)
 
-    return pval
+    return pval, obs, dist
 
 if __name__ == "__main__":
     # small test
